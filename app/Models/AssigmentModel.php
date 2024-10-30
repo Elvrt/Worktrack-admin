0c367f5edf7f6e6c3f60a070b143afc32ddd3382
andybima6 <andybima6@gmail.com>
2024-10-30 09:16:08 +0700

    model

--- app/Models/AssigmentModel.php
+++ app/Models/AssigmentModel.php
@@ -5,22 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Assignment extends Model
+class AssigmentModel extends Model
 {
     use HasFactory;
-
+    protected $table = 'assigment';
     protected $primaryKey = 'assignment_id';
     protected $fillable = ['employee_id', 'goal_id'];
 
     // Relasi ke Employee
     public function employee()
     {
-        return $this->belongsTo(Employee::class, 'employee_id');
+        return $this->belongsTo(EmployeeModel::class, 'employee_id');
     }
 
     // Relasi ke Goal
     public function goal()
     {
-        return $this->belongsTo(Goal::class, 'goal_id');
+        return $this->belongsTo(GoalModel::class, 'goal_id');
     }
 }
