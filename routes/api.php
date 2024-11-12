3c5de36b6dec8778fe7b8c2fb050955b0fb9f35e
Elvrt <elvarettas23@gmail.com>
2024-11-12 19:08:16 +0700

    api timeoff

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\APIAuthController;
-
+use App\Http\Controllers\APIEmployeeController;
+use App\Http\Controllers\APITimeOffController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,3 +26,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [APIAuthController::class, 'logout']);
 });
 
+Route::group(['prefix' => 'timeoff'], function () {
+    Route::get('/', [APITimeOffController::class, 'index']);
+    Route::post('/store', [APITimeOffController::class, 'store']);
+    Route::get('/show/{id}', [APITimeOffController::class, 'show']);
+    Route::post('/edit/{id}', [APITimeOffController::class, 'edit']);
+    Route::put('/update/{id}', [APITimeOffController::class, 'update']);
+    Route::delete('/delete/{id}', [APITimeOffController::class, 'destroy']);
+});
+
+// Route::post("create_data", [APITimeOffController::
